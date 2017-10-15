b156e57c1d0cc534bea53e4acfa7fe4b41c3e1e9
Varun Bharadwaj <varunbharadwaj1995@gmail.com>
2017-10-15 23:36:47 +0530

    Update CrowdSimulator.py

--- CrowdSimulator.py
+++ CrowdSimulator.py
@@ -3,9 +3,6 @@ from HeatMap import HeatMap
 
 # simulate crowd by generating coordinates representing people
 class CrowdSimulator:
-	groundValue = 10
-	highValueRange = (80,100)
-
 	# height and width of the place (same as that of image captured by cameras)
 	# changerate - rate at which ppl move out of the place
 	def __init__(self, sizeX=1024, sizeY=1024, changeRate=10):
