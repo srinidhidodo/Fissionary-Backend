7d4af3ed4aa8724a5ef9c04d0888b00a7720ee3d
HarshithUK <harshithuk.acharya@gmail.com>
2017-11-13 21:01:45 +0530

    Added Configuration Files and made changes to the original database program

--- database.py
+++ database.py
@@ -6,42 +6,63 @@ import time
 def index():
     conn = sqlite3.connect('database.db')
     c = conn.cursor()
-    c.execute('''CREATE TABLE PLACE(time text, location text, heatmap text)''')
-    '''
-        TIME -> A string for timestamp [Ex : 'Sun Oct 22 12:17:45 2017']
-        LOCATION -> A string for location [Ex : 'B-Block','Student Lounge']
-        HEATMAP -> A string which is the name of the heatmap file generated [Ex : 'heatmap1.png']
-    '''
+    #c.execute('''ALTER TABLE PLACE ADD FOREIGN KEY pid(pid) REFERENCES parent_table(columns)''')
+    c.execute('''CREATE TABLE LOCATION(lid text primary key, lname text, lat decimal(8,6), lon decimal(9,6))''')
+    c.execute('''CREATE TABLE LOC_PLACE(pid text primary key, lid text, pname text, lat decimal(8,6), lon decimal(9,6), threshold integer, foreign key (lid) references LOCATION(lid))''')
+    c.execute('''CREATE TABLE PLACE(pid text primary key, time text, heatmap text, foreign key (pid) references LOC_PLACE(pid))''')
+    #INSERT VALUES INTO TABLE LOCATION AND LOC_PLACE
+    f1 = open('config_loc.txt','r')
+    for line in f1.read():
+        w = line.split(';')
+        c.execute('''INSERT INTO LOCATION VALUES(?,?,?,?)''',w)
+    f2 = open('config_place.txt','r')
+    for line in f2.read():
+        w = line.split(';')
+        c.execute('''INSERT INTO LOC_PLACE VALUES(?,?,?,?,?,?)''',w)
     conn.commit()
     conn.close()
-    return render_template('index.html') # Go to Frontend home web page
+    #return render_template('index.html') # Go to Frontend home web page
 
-@app.route("/retrieve",methods = ['POST'])
-def retreive():
+#retrieve only place and location
+@app.route("/retrieve_loc",methods = ['POST'])
+def retreive_loc():
     conn = sqlite3.connect('database.db')
     c = conn.cursor()
     loc = request.form['loc']
     #time = request.form['time']
     #params = (loc,time,)
     #c.execute('''SELECT * FROM PLACE WHERE location = ? AND time = ?''',)
-    c.execute('''SELECT * FROM PLACE WHERE location = ''',loc)
+    c.execute('''SELECT * FROM LOCATION WHERE location = ''',loc)
     r = c.fetchone()
-    tm = r[0]
-    hm = r[2]
+    #tm = r[0]
+    #hm = r[2]
     conn.close()
-    return render_template('heatmap.html',heatmap=hm,time=tm) #returning back to the required html page
+    return r 
+    #return render_template('heatmap.html',heatmap=hm,time=tm) #returning back to the required html page
 
-@app.route("/store",methods = ['POST'])
-def store():
+@app.route("/retrieve_plc",methods=['POST'])
+def retrieve_plc():
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    plc = request.form['plc']
+    c.execute('''SELECT * FROM LOC_PLACE WHERE plc = ''',plc)
+    r = c.fetchone()
+    #tm = r[0]
+    #hm = r[2]
+    conn.close()
+    return r
+
+@app.route("/update",methods = ['POST'])
+def update():
     conn = sqlite3.connect('database.db')
     c = conn.cursor()
     loc = request.form['loc']
     heatmap = request.form['hm']
-    params = (time.ctime(),loc,heatmap)
+    params = (loc,time.ctime(),heatmap)
     c.execute('''INSERT INTO PLACE VALUES(?,?,?)''',params) #inserting to the database
     conn.commit()
     conn.close()
-    return render_template('index.html')
+    #return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(
